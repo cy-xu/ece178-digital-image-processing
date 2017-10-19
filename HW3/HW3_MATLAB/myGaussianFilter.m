f41907e088c18b1ca8a27c498227835157ac662b
cy-xu <hello.xu@gmail.com>
2017-10-18 23:25:54 -0700

    first push

---
+++ HW3/HW3_MATLAB/myGaussianFilter.m
@@ -0,0 +1,10 @@
+function filter = myGaussianFilter(height,width,mu,sigma)
+% height   : height of the filter kernel matrix
+% width    : width of the filter kernel matrix
+% mu       : mean 
+% sigma    : standard deviation
+% filter   : output filter kernel matrix of type double
+
+%% implement your Gaussian filter here
+
+% test git control
---
+++ HW3/HW3_MATLAB/myIDCT_Transform.m
@@ -0,0 +1,7 @@
+function rImg = myIDCT_Transform(dctImg)
+% dctImg      : DCT transform of the input image (type: double)
+% rImg        : recovered image after performing inverse DCT (type: double)
+
+%% implement your inverse DCT transform here
+
+
