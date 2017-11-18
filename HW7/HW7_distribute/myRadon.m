3aa854aa17d7cd1f273e545f6f02e670bf36952a
CY Xu <hello.xu@gmail.com>
2017-11-17 21:40:39 -0800

    HW7 uploaded

---
+++ HW7/HW7_distribute/myRadon.m
@@ -0,0 +1,5 @@
+function [PR] = myRadon(img, theta_all)
+% img: input image (type double)
+% theta_all: list of angles in degrees (length=K)
+% PR: radon transform (type double)(size: N x K) 
+%% your implementation here
